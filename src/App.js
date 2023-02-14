b866e1b565ed233645b9eb3a22fbc2b0bff15908
Uzair <47213777+Uzair05@users.noreply.github.com>
2023-02-14 16:15:02 +0800

    dynamic rendering

--- src/App.js
+++ src/App.js
@@ -27,6 +27,54 @@ function App() {
       },
       metric: "ppm",
     },
+    {
+      reading: 750,
+      bar: {
+        left: { status: "Excellent", value: "<800" },
+        right: { status: "Good", value: "<1000" },
+      },
+      metric: "ppm",
+    },
+    {
+      reading: 905,
+      bar: {
+        left: { status: "Good", value: "<1000" },
+        right: { status: "Normal", value: "<1500" },
+      },
+      metric: "ppm",
+    },
+    {
+      reading: 1400,
+      bar: {
+        left: { status: "Normal", value: "<1500" },
+        right: { status: "Offline", value: null },
+      },
+      metric: "ppm",
+    },
+    {
+      reading: 750,
+      bar: {
+        left: { status: "Excellent", value: "<800" },
+        right: { status: "Good", value: "<1000" },
+      },
+      metric: "ppm",
+    },
+    {
+      reading: 905,
+      bar: {
+        left: { status: "Good", value: "<1000" },
+        right: { status: "Normal", value: "<1500" },
+      },
+      metric: "ppm",
+    },
+    {
+      reading: 1400,
+      bar: {
+        left: { status: "Normal", value: "<1500" },
+        right: { status: "Offline", value: null },
+      },
+      metric: "ppm",
+    },
   ];
   return <DisplayBoard items={items} />;
 }
