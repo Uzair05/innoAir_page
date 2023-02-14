5019ddd14674484411fd336b0869b9b116c1c60e
Uzair <47213777+Uzair05@users.noreply.github.com>
2023-02-14 15:06:53 +0800

    Fix component styling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import DataDisplayCard from "./components/DataDisplayCard";
 
 function App() {
   return (
-    <DataDisplayCard
-      reading={25}
-      bar={{
-        left: { status: "Excellent", value: "<800" },
-        right: { status: "Good", value: "<1000" },
-      }}
-    />
+    <div>
+      <DataDisplayCard
+        reading={25}
+        bar={{
+          left: { status: "Excellent", value: "<800" },
+          right: { status: "Good", value: "<1000" },
+        }}
+      />
+      <DataDisplayCard
+        reading={25}
+        bar={{
+          left: { status: "Good", value: "<1000" },
+          right: { status: "Normal", value: "<1500" },
+        }}
+      />
+    </div>
   );
 }
 
--- src/components/DataDisplayCard.js
+++ src/components/DataDisplayCard.js
@@ -1,23 +1,25 @@
 import "./DataDisplayCard.css";
 
 function DataDisplayCard(props) {
-  const color_class = {
-    Excellent: "HorizontalBar_excellent",
-    Good: "HorizontalBar_good",
-    Normal: "HorizontalBar_normal",
-    Offline: "HorizontalBar_offline",
+  const createColor = (color_) => {
+    return "_".concat(color_.toLowerCase());
   };
 
   return (
-    <div className="OuterCircle">
+    <div
+      className={"OuterCircle"
+        .concat(" Outer")
+        .concat(createColor(props.bar.left.status))}
+    >
       <div className="InnerCircle">
         <p className="InnerText">{props.reading}</p>
       </div>
+
       <div className="HorizontalBar">
         <div
-          className={"HorizontalBarLeft".concat(
-            " ".concat(color_class[props.bar.left.status])
-          )}
+          className={"HorizontalBarLeft"
+            .concat(" HorizontalBar")
+            .concat(createColor(props.bar.left.status))}
         >
           <p className="HorizontalBar_text">
             {props.bar.left.status}
@@ -26,9 +28,9 @@ function DataDisplayCard(props) {
           </p>
         </div>
         <div
-          className={"HorizontalBarRight".concat(
-            " ".concat(color_class[props.bar.right.status])
-          )}
+          className={"HorizontalBarRight"
+            .concat(" HorizontalBar")
+            .concat(createColor(props.bar.right.status))}
         >
           <p className="HorizontalBar_text">
             {props.bar.right.status}
