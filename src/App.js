e02a7837ef4298a6d5ce2d840d5118db8ad4cc9d
Uzair <47213777+Uzair05@users.noreply.github.com>
2023-02-14 16:13:37 +0800

    dynamic rendering

--- src/App.js
+++ src/App.js
@@ -1,53 +1,36 @@
 import "./components/DataDisplayCard";
 import "./App.css";
-import DataDisplayCard from "./components/DataDisplayCard";
 import DisplayBoard from "./components/DisplayBoard";
+import DataDisplayCard from "./components/DataDisplayCard";
 
 function App() {
-  return (
-    <DisplayBoard>
-      <DataDisplayCard
-        reading={750}
-        bar={{
-          left: { status: "Excellent", value: "<800" },
-          right: { status: "Good", value: "<1000" },
-        }}
-        metric={"ppm"}
-      />
-      <DataDisplayCard
-        reading={905}
-        bar={{
-          left: { status: "Good", value: "<1000" },
-          right: { status: "Normal", value: "<1500" },
-        }}
-        metric={"ppm"}
-      />
-      <DataDisplayCard
-        reading={1400}
-        bar={{
-          left: { status: "Normal", value: "<1500" },
-          right: { status: "Offline", value: null },
-        }}
-        metric={"ppm"}
-      />
-      <DataDisplayCard
-        reading={1400}
-        bar={{
-          left: { status: "Normal", value: "<1500" },
-          right: { status: "Offline", value: null },
-        }}
-        metric={"ppm"}
-      />
-      <DataDisplayCard
-        reading={1400}
-        bar={{
-          left: { status: "Normal", value: "<1500" },
-          right: { status: "Offline", value: null },
-        }}
-        metric={"ppm"}
-      />
-    </DisplayBoard>
-  );
+  const items = [
+    {
+      reading: 750,
+      bar: {
+        left: { status: "Excellent", value: "<800" },
+        right: { status: "Good", value: "<1000" },
+      },
+      metric: "ppm",
+    },
+    {
+      reading: 905,
+      bar: {
+        left: { status: "Good", value: "<1000" },
+        right: { status: "Normal", value: "<1500" },
+      },
+      metric: "ppm",
+    },
+    {
+      reading: 1400,
+      bar: {
+        left: { status: "Normal", value: "<1500" },
+        right: { status: "Offline", value: null },
+      },
+      metric: "ppm",
+    },
+  ];
+  return <DisplayBoard items={items} />;
 }
 
 export default App;
--- src/components/DisplayBoard.js
+++ src/components/DisplayBoard.js
@@ -1,7 +1,20 @@
 import "./DisplayBoard.css";
+import DataDisplayCard from "./DataDisplayCard";
 
 function DisplayBoard(props) {
-  return <div className="displayBoard">{props.children}</div>;
+  return (
+    <div>
+      {props.items.map((item) => {
+        return (
+          <DataDisplayCard
+            metric={item.metric}
+            bar={item.bar}
+            reading={item.reading}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default DisplayBoard;
