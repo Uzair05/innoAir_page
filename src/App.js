a3946a708b04f2dd1e225e540c8e780ade97e981
Uzair <47213777+Uzair05@users.noreply.github.com>
2023-02-17 15:42:25 +0800

    moving

--- src/App.js
+++ src/App.js
@@ -1,84 +1,51 @@
+import { useState } from "react";
 import "./App.css";
 import DisplayBoard from "./components/DisplayBoard";
 
 function App() {
-  const items = [
-    {
-      title: "Dust",
-      reading: 750,
-      bar: {
-        left: { status: "Excellent", value: "<800" },
-        right: { status: "Good", value: "<1000" },
-      },
-      metric: "ppm",
-    },
-    {
-      reading: 905,
-      bar: {
-        left: { status: "Good", value: "<1000" },
-        right: { status: "Normal", value: "<1500" },
-      },
-      metric: "ppm",
-    },
-    {
-      reading: 1400,
-      bar: {
-        left: { status: "Normal", value: "<1500" },
-        right: { status: "Offline", value: null },
-      },
-      metric: "ppm",
-    },
-    {
-      reading: 750,
-      bar: {
-        left: { status: "Excellent", value: "<800" },
-        right: { status: "Good", value: "<1000" },
-      },
-      metric: "ppm",
-    },
-    {
-      reading: 905,
-      bar: {
-        left: { status: "Good", value: "<1000" },
-        right: { status: "Normal", value: "<1500" },
-      },
-      metric: "ppm",
-    },
-    {
-      reading: 1400,
-      bar: {
-        left: { status: "Normal", value: "<1500" },
-        right: { status: "Offline", value: null },
-      },
-      metric: "ppm",
-    },
-    {
-      reading: 750,
-      bar: {
-        left: { status: "Excellent", value: "<800" },
-        right: { status: "Good", value: "<1000" },
-      },
-      metric: "ppm",
-    },
-    {
-      reading: 905,
-      bar: {
-        left: { status: "Good", value: "<1000" },
-        right: { status: "Normal", value: "<1500" },
-      },
-      metric: "ppm",
-    },
-    {
-      reading: 1400,
-      bar: {
-        left: { status: "Normal", value: "<1500" },
-        right: { status: "Offline", value: null },
-      },
-      metric: "ppm",
-    },
-  ];
+  const [items, setData] = useState([]);
 
-  return <DisplayBoard items={items} />;
+  async function fetchAPI() {
+    const fr_time = new Date();
+    fr_time.setTime(fr_time.getTime() - 1 * 60 * 1000);
+    const to_time = new Date();
+
+    const response = await fetch(
+      `https://1v2kgpsm3a.execute-api.ap-northeast-2.amazonaws.com/innoair/I01A002F001B?interval=${0}&from_time=${fr_time.toISOString()}&to_time=${to_time.toISOString()}`
+    );
+    const data = await response.json();
+    setData(data);
+
+    let items = [];
+    for (const [key, value] of Object.entries(data)) {
+      if (keyTitle[key] !== undefined) {
+        items.push({
+          title: keyTitle[key],
+          reading: value[0],
+          metric: keyMetric[key],
+          graph_link: "",
+          bar: {
+            left: {
+              status: keyClass[key](value[0]),
+              value: keyClassDefinition[key],
+            },
+            right: {
+              status:
+                keyClass[key](value[0]) === "Excellent" ? "Good" : "Normal",
+              value: keyClassDefinition[key],
+            },
+          },
+        });
+      }
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={fetchAPI}>Load Page</button>
+      <p>{JSON.stringify(items)}</p>
+    </div>
+  );
 }
 
 export default App;
